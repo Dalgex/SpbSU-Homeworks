79ec2611adef3c8a1b2a5a8b5d231b5c03d0107b
Valeriy Karnaukhov <Valera-96@yandex.ru>
2015-03-18 21:18:27 +0300

    Task 5.3

---
+++ 2 semester/5 homework/Task 5.3/FoldTest/FoldTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Task_5._3;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FoldTest
+{
+    [TestClass]
+    public class FoldTest
+    {
+        private List<int> list1;
+        private List<int> list2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list1 = new List<int> { 4, 6, 3 };
+            list2 = new List<int> { 3, 2, 5, 4 };
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int fold = new FoldClass<int>().Fold(list1, 3, (x, y) => x * y);
+            Assert.AreEqual(fold, 3 * list1[0] * list1[1] * list1[2]);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int fold = new FoldClass<int>().Fold(list2, 7, (x, y) => x * y);
+            Assert.IsTrue(fold == 7 * list2[0] * list2[1] * list2[2] * list2[3]);
+        }
+    }
+}
---
+++ 2 semester/5 homework/Task 5.3/Task 5.3/Fold.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._3
+{
+    public class FoldClass<T>
+    {
+        public T Fold(List<T> list, T value, Func<T, T, T> function)
+        {
+            List<int> foldList = new List<int>();
+
+            foreach (var temp in list)
+            {
+                value = function(value, temp);
+            }
+
+            return value;
+        }
+    }
+}
