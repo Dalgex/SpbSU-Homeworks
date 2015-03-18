f1c720a09d61c0f3334589be593cf26e58d68cd7
Valeriy Karnaukhov <Valera-96@yandex.ru>
2015-03-18 21:14:00 +0300

    Task 5.2

---
+++ 2 semester/5 homework/Task 5.2/FilterTest/FilterTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Task_5._2;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FilterTest
+{
+    [TestClass]
+    public class FilterTest
+    {
+        private List<int> list;
+        private List<int> filter1;
+        private List<int> filter2;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int> { 1, 10, 6, 3 };
+            filter1 = new FilterClass<int>().Filter(list, x => x % 2 == 0);
+            filter2 = new FilterClass<int>().Filter(list, x => x % 3 == 1);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(filter1[0] == 10 && filter1[1] == 6 && filter1.Count == 2);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(filter2[0] == 1 && filter2[1] == 10 && filter2.Count == 2);
+        }
+    }
+}
---
+++ 2 semester/5 homework/Task 5.2/Task 5.2/Filter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._2
+{
+    public class FilterClass<T>
+    {
+        public List<T> Filter(List<T> list, Func<T, bool> function)
+        {
+            List<T> filterList = new List<T>();
+
+            foreach (var temp in list)
+            {
+                if (function(temp))
+                {
+                    filterList.Add(temp);
+                }
+            }
+
+            return filterList;
+        }
+    }
+}
