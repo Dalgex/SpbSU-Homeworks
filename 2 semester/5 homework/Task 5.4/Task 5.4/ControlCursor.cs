01f9d672fbbcccf331706f0a5ab5890cbd5d044c
Valeriy Karnaukhov <Valera-96@yandex.ru>
2015-03-25 22:45:59 +0300

    Task 5.4

---
+++ 2 semester/5 homework/Task 5.4/Task 5.4/ControlCursor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._4
+{
+    public class ControlCursor
+    {
+        public void Right(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft < Console.BufferWidth - 1)
+            {
+                Console.CursorLeft += 1;
+            }
+        }
+
+        public void Left(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft > 0)
+            {
+                Console.CursorLeft -= 1;
+            }
+        }
+
+        public void Down(object sender, EventArgs args)
+        {
+            if (Console.CursorTop < Console.BufferHeight - 1)
+            {
+                Console.CursorTop += 1;
+            }
+        }
+
+        public void Up(object sender, EventArgs args)
+        {
+            if (Console.CursorTop > 0)
+            {
+                Console.CursorTop -= 1;
+            }
+        }
+    }
+}
