ac07b610bd62804cc9de0c9cbd026d8c1ce5ae72
Valeriy Karnaukhov <Valera-96@yandex.ru>
2015-04-03 15:16:21 +0300

    Test 1

---
+++ 2 semester/Test 1/Test 1/QueueTest/QueueTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Test_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QueueTest
+{
+    [TestClass]
+    public class QueueTest
+    {
+        private PriorityQueue<int> queue;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            queue = new PriorityQueue<int>();
+        }
+
+        [TestMethod]
+        public void EnqueueTest()
+        {
+            queue.Enqueue(5, 8);
+            Assert.AreEqual(1, queue.Count);
+            queue.Enqueue(3, 6);
+            queue.Enqueue(7, 9);
+            Assert.AreEqual(3, queue.Count);
+        }
+
+        [TestMethod]
+        public void DequeueTest()
+        {
+            queue.Enqueue(2, 8);
+            queue.Enqueue(5, 5);
+            queue.Enqueue(4, 8);
+            queue.Enqueue(7, 6);
+            Assert.AreEqual(2, queue.Dequeue());
+            Assert.AreEqual(4, queue.Dequeue());
+            Assert.AreEqual(7, queue.Dequeue());
+            queue.Enqueue(1, 4);
+            queue.Enqueue(3, 10);
+            Assert.AreEqual(3, queue.Dequeue());
+            queue.Enqueue(4, 2);
+            Assert.AreEqual(5, queue.Dequeue());
+            queue.Enqueue(9, 8);
+            Assert.AreEqual(9, queue.Dequeue());
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(4, queue.Dequeue());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddElements()
+        {
+            queue.Enqueue(5, 8);
+            queue.Dequeue();
+            queue.Dequeue();
+        }
+    }
+}
---
+++ 2 semester/Test 1/Test 1/Test 1/Queue.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_1
+{
+    /// <summary>
+    /// Класс очередь с приоритетами
+    /// </summary>
+    public class PriorityQueue<T>
+    {
+        class QueueElement
+        {
+            public QueueElement Next { get; set; }
+            public QueueElement Previous { get; set; }
+            public T Value { get; private set; }
+            public int Priority { get; private set; }
+
+            public QueueElement(T value, int priority)
+            {
+                Priority = priority;
+                Value = value;
+            }
+        }
+
+        private QueueElement head;
+        private QueueElement tail;
+        private int highestPriority; // самый высокий приоритет
+
+        /// <summary>
+        /// Число элементов, содержащихся в очереди
+        /// </summary>
+        public int Count { get; private set; } 
+
+        /// <summary>
+        /// Принимает на вход значение и численный приоритет и добавляет элемент в очередь
+        /// </summary>
+        public void Enqueue(T value, int priority)
+        {
+            QueueElement newElement = new QueueElement(value, priority);
+            newElement.Previous = tail;
+
+            if (Count > 0)
+            {
+                tail.Next = newElement;
+            }
+            else
+            {
+                head = newElement;
+            }
+
+            tail = newElement;
+            Count++;
+
+            if (newElement.Priority > highestPriority)
+            {
+                highestPriority = newElement.Priority;
+            }
+        }
+
+        /// <summary>
+        /// Поиск элемента с самым высоким приоритетом
+        /// </summary>
+        public void newHighestPriority()
+        {
+            if (Count == 0)
+            {
+                highestPriority = 0;
+                return;
+            }
+
+            QueueElement current = head;
+            highestPriority = head.Priority;
+
+            while (current != null)
+            {
+                if (current.Priority > highestPriority)
+                {
+                    highestPriority = current.Priority;
+                }
+
+                current = current.Next;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает значение с наивысшим приоритетом и удаляет его из оереди
+        /// </summary>
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            QueueElement current = head;
+
+            while (current.Next != null)
+            {
+                if (current.Priority == highestPriority)
+                {
+                    break;
+                }
+
+                current = current.Next;
+            }
+
+            Count--;
+
+            if (current == head)
+            {
+                head = head.Next;
+                newHighestPriority();
+                return current.Value;
+            }
+
+            if (current.Next == null)
+            {
+                tail.Previous.Next = null;
+                tail = current.Previous;
+                newHighestPriority();
+                return current.Value;
+            }
+
+            current.Previous.Next = current.Next;
+            current.Next.Previous = current.Previous;
+            newHighestPriority();
+            return current.Value;
+        }
+    }
+}
