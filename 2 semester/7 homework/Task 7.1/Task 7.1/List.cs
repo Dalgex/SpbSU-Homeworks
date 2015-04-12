48cf83c35835f1a6ef76daf1009e7c325dd1778a
Valeriy Karnaukhov <Valera-96@yandex.ru>
2015-04-13 00:28:04 +0300

    Task 7.1

---
+++ 2 semester/7 homework/Task 7.1/Task 7.1/List.cs
@@ -0,0 +1,493 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7._1
+{
+    /// <summary>
+    /// Представляет строго типизированный список объектов, доступных по индексу
+    /// </summary>
+    public class List<T> : IEnumerable<T>
+    {
+        class ListElement
+        {
+            public ListElement Next { get; set; }
+            public ListElement Previous { get; set; }
+            public T Value { get; set; }
+
+            public ListElement(T value)
+            {
+                Value = value;
+            }
+        }
+
+        private ListElement head;
+        private ListElement tail;
+        private int size;
+
+        /// <summary>
+        /// Получает число элементов, содержащихся в коллекции List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public int Count
+        {
+            get { return size; }
+        }
+
+        /// <summary>
+        /// Получает или задает элемент с указанным индексом
+        /// </summary>
+        public T this[int index]
+        {
+            get { return FindElementByIndex(index).Value; }
+            set { FindElementByIndex(index).Value = value; }
+        }
+
+        /// <summary>
+        /// Исключение, которое выбрасывается, при передаче указателя null
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        private void CheckArgumentNullException(Predicate<T> match)
+        {
+            if (match == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
+
+        /// <summary>
+        /// Исключение, которое выбрасывается, когда значение аргумента находится вне допустимого диапазона значений
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        private void CheckArgumentOutOfRangeException(int index)
+        {
+            if (index < 0 || index > Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        /// <summary>
+        /// Добавляет объект в конец коллекции List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public void Add(T value)
+        {
+            var newElement = new ListElement(value);
+            newElement.Previous = tail;
+
+            if (Count > 0)
+            {
+                tail.Next = newElement;
+            }
+            else
+            {
+                head = newElement;
+            }
+
+            tail = newElement;
+            size++;
+        }
+
+        /// <summary>
+        /// Удаляет все элементы из коллекции List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        /// <summary>
+        /// Определяет, входит ли элемент в состав List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public bool Contains(T value)
+        {
+            return IndexOf(value) != -1;
+        }
+
+        /// <summary>
+        /// Копирует List<typeparamref name="&lt;T&gt;"/> целиком в совместимый одномерный массив, 
+        /// начиная с указанного индекса конечного массива
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (array.Length - arrayIndex < size)
+            {
+                throw new ArgumentException();
+            }
+
+            Array.Copy(this.ToArray(), 0, array, arrayIndex, Count);
+        }
+
+        /// <summary>
+        /// Определяет, содержит ли List<typeparamref name="&lt;T&gt;"/> элементы, удовлетворяющие условиям указанного предиката
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public bool Exists(Predicate<T> match)
+        {
+            return FindIndex(match) != -1;
+        }
+
+        /// <summary>
+        /// Выполняет поиск элемента, удовлетворяющего условиям указанного предиката, 
+        /// и возвращает первое найденное вхождение в пределах всего списка List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public T Find(Predicate<T> match)
+        {
+            CheckArgumentNullException(match);
+
+            foreach (var value in this)
+            {
+                if (match(value))
+                {
+                    return value;
+                }
+            }
+
+            return default(T); // Если не найден элемент, удовлетворяющий условиям предиката, возвращает значение по умолчанию для типа T
+        }
+
+        /// <summary>
+        /// Извлекает все элементы, удовлетворяющие условиям указанного предиката
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public List<T> FindAll(Predicate<T> match)
+        {
+            CheckArgumentNullException(match);
+
+            List<T> list = new List<T>();
+
+            foreach (var value in this)
+            {
+                if (match(value))
+                {
+                    list.Add(value);
+                }
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Выполняет поиск элемента, удовлетворяющего условиям указанного предиката, и возвращает отсчитываемый от нуля 
+        /// индекс первого найденного вхождения в пределах всего списка List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        /// <exception cref="ArgumentNullException"></exception>
+        public int FindIndex(Predicate<T> match)
+        {
+            CheckArgumentNullException(match);
+
+            int count = 0;
+
+            foreach (var value in this)
+            {
+                if (match(value))
+                {
+                    return count;
+                }
+
+                count++;
+            }
+
+            return -1; // Если не найден элемент, удовлетворяющий условиям предиката match, возвращает -1
+        }
+
+        /// <summary>
+        /// Выполняет поиск элемента по индексу в коллекции List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        private ListElement FindElementByIndex(int index) // этот метод нужен только самому классу List<T>
+        {
+            CheckArgumentOutOfRangeException(index);
+
+            var current = head;
+
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            return current;
+        }
+
+        /// <summary>
+        /// Осуществляет поиск указанного объекта и возвращает отсчитываемый от нуля индекс первого вхождения,
+        /// найденного в пределах всего списка List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public int IndexOf(T value)
+        {
+            var current = head;
+            int count = 0;
+
+            while (current != null)
+            {
+                if (Equals(current.Value, value))
+                {
+                    return count;
+                }
+
+                count++;
+                current = current.Next;
+            }
+
+            return -1;  // Если элемент не найден, возвращает -1
+        }
+
+        /// <summary>
+        /// Добавляет объект в начало коллекции List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        private void InsertToHead(T value)  // данный метод нужен только самому классу List<T>
+        {
+            var newElement = new ListElement(value);
+            newElement.Next = head;
+
+            if (Count > 0)
+            {
+                head.Previous = newElement;
+            }
+            else
+            {
+                tail = newElement;
+            }
+
+            head = newElement;
+            size++;
+        }
+
+        /// <summary>
+        /// Добавляет элемент в список List<typeparamref name="&lt;T&gt;"/> в позиции с указанным индексом
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Insert(int index, T value)
+        {
+            CheckArgumentOutOfRangeException(index);
+
+            if (index == 0)
+            {
+                InsertToHead(value);
+                return;
+            }
+
+            if (index == Count)
+            {
+                Add(value);
+                return;
+            }
+
+            var newElement = new ListElement(value);
+            var current = head;
+
+            while (index != 0)
+            {
+                current = current.Next;
+                index--;
+            }
+
+            newElement.Next = current;
+            newElement.Previous = current.Previous;
+            current.Previous.Next = newElement;
+            current.Previous = newElement;
+            size++;
+        }
+
+        /// <summary>
+        /// Изменяет порядок элементов во всем списке List<typeparamref name="&lt;T&gt;"/> на обратный
+        /// </summary>
+        public void Reverse()
+        {
+            List<T> list = new List<T>();
+
+            foreach (var value in this)
+            {
+                list.Insert(0, value);
+            }
+
+            head = list.head;
+            tail = list.tail;
+        }
+
+        /// <summary>
+        /// Удаляет первое вхождение указанного объекта из коллекции List<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public bool Remove(T value)
+        {
+            int index = IndexOf(value);
+
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Удаляет элемент списка List<typeparamref name="&lt;T&gt;"/> с указанным индексом
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void RemoveAt(int index)
+        {
+            CheckArgumentOutOfRangeException(index);
+
+            size--;
+
+            if (index == 0)
+            {
+                head = head.Next;
+                return;
+            }
+
+            if (index == Count)
+            {
+                tail.Previous.Next = null;
+                tail = tail.Previous;
+                return;
+            }
+
+            var current = head;
+
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+
+            current.Previous.Next = current.Next;
+            current.Next.Previous = current.Previous;
+        }
+        
+        /// <summary>
+        /// Копирует элементы списка List<typeparamref name="&lt;T&gt;"/> в новый массив
+        /// </summary>
+        public T[] ToArray()
+        {
+            T[] array = new T[Count];
+            int i = 0;
+
+            foreach (var value in this)
+            {
+                array[i] = value;
+                i++;
+            }
+
+            return array;
+        } 
+
+        /// <summary>
+        /// Возвращает строку, состоящую из всех элементов списка
+        /// </summary>
+        public override string ToString()
+        {
+            string result = String.Empty;
+
+            foreach (var value in this)
+            {
+                result += value + " ";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Определяет, все ли элементы списка List<typeparamref name="&lt;T&gt;"/> удовлетворяют условиям указанного предиката
+        /// </summary>
+        public bool TrueForAll(Predicate<T> match)
+        {
+            CheckArgumentNullException(match);
+
+            foreach (var value in this)
+            {
+                if (!match(value))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Возвращает перечислитель, выполняющий перебор элементов коллекции
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new ListEnumerator(this);
+        }
+
+        /// <summary>
+        /// Возвращает перечислитель, осуществляющий перебор коллекции
+        /// </summary>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Реализует простой перебор элементов универсальной коллекции
+        /// </summary>
+        private class ListEnumerator : IEnumerator<T>
+        {
+            private int iterator = -1; // перечислитель
+            private List<T> list;
+
+            public ListEnumerator(List<T> list)
+            {
+                this.list = list;
+            }
+
+            /// <summary>
+            /// Получает текущий элемент в коллекции
+            /// </summary>
+            public object Current
+            {
+                get { return list.FindElementByIndex(iterator); }
+            }
+
+            /// <summary>
+            /// Получает элемент коллекции, соответствующий текущей позиции перечислителя
+            /// </summary>
+            T IEnumerator<T>.Current
+            {
+                get { return list[iterator]; }
+            }
+
+            /// <summary>
+            /// Устанавливает перечислитель в его начальное положение, т.е. перед первым элементом коллекции
+            /// </summary>
+            public void Reset()
+            {
+                iterator = -1;
+            }
+
+            /// <summary>
+            /// Перемещает перечислитель к следующему элементу коллекции
+            /// </summary>
+            public bool MoveNext()
+            {
+                iterator++;
+                return !Equals(list.FindElementByIndex(iterator), null);
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+    }
+}
---
+++ 2 semester/7 homework/Task 7.1/Task 7.1/Stack.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7._1
+{
+    /// <summary>
+    /// Представляет коллекцию объектов, действующую по принципу: последним пришел - первым вышел
+    /// </summary>
+    public class Stack<T>
+    {
+        class StackElement
+        {
+            public T Value { get; private set; }
+            public StackElement Next { get; private set; }
+
+            public StackElement(T value, StackElement next)
+            {
+                Next = next;
+                Value = value;
+            }
+        }
+
+        private StackElement top;
+        private int size;
+
+        /// <summary>
+        /// Получает число элементов, содержащихся в коллекции Stack<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public int Count
+        {
+            get { return size; }
+        }
+
+        /// <summary>
+        /// Вставляет объект в верхнюю часть Stack<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public void Push(T value)
+        {
+            StackElement newElement = new StackElement(value, top);
+            top = newElement;
+            size++;
+        }
+
+        /// <summary>
+        /// Возвращает самый верхний объект Stack<typeparamref name="&lt;T&gt;"/>, но не удаляет его
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Стек пуст");
+            }
+
+            return top.Value;
+        }
+
+        /// <summary>
+        /// Определяет, находится ли элемент в Stack<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public bool Contains(T value)
+        {
+            var current = top;
+
+            while (current != null)
+            {
+                if (Equals(current.Value, value))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Удаляет и возвращает объект из верхней части Stack<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Стек пуст");
+            }
+
+            T result = top.Value;
+            top = top.Next;
+            size--;
+            return result;
+        }
+
+        /// <summary>
+        /// Удаляет все объекты из Stack<typeparamref name="&lt;T&gt;"/>
+        /// </summary>
+        public void Clear()
+        {
+            top = null;
+            size = 0;
+        }
+    }
+}
