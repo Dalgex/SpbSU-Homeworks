c10a3f87a5a15a31a787a0a3c82a4397ca1ee5ee
Valeriy Karnaukhov <Valera-96@yandex.ru>
2015-12-23 20:25:54 +0300

    New classes PaintingInAction and ShapeBuilder were added. Now, events, associated with the mouse pointer, call appropriate methods of the new class PaintingInAction.

--- 3 semester/4 homework/Editor/Editor/Editor.cs
+++ 3 semester/4 homework/Editor/Editor/Editor.cs
@@ -12,131 +12,45 @@ namespace Editor
 {
     public partial class Editor : Form
     {
-        private List<Shape> shapes = new List<Shape>(); // создаем список, в котором будем хранить нарисованные линии
-        private History history = new History(); // создаем историю, где будем хранить операции (команды)
-        private ActionsOnShapes actions = new ActionsOnShapes(); // создаем объект действия над фигурами, который будет отвечать за удаление всех фигур
+        private List<Shape> shapes = new List<Shape>();
+        private History history = new History();
+        private ActionsOnShapes actions = new ActionsOnShapes();
+        private PaintingInAction paintingInAction;
 
         private Color color = Color.Black; // задаем изначальный цвет линий
         private int width = 2; // задаем изначальную толщину линий
 
-        private Color colorOfChangeLine; // представляет цвет линии, которую перемещаем
-        private int widthOfChangeLine; // представляет толщину линии, которую перемещаем
-        private bool wasMovedLine; // была ли изменена линия, т.е. перемещена
-
-        private bool isPressed; // была ли нажата кнопка мыши
-
-        private int beginX; // координата начала линии по X
-        private int beginY; // координата начала линии по Y
-
-        private int endX; // координата конца линии по X
-        private int endY; // координата конца линии по Y
-
-        private int lastX; // эти переменные нужны для того, чтобы не отображать точки, т.е. не считать клик мышки за новую линию
-        private int lastY;
-
-        private bool isLastWasRedo; // последняя операция была Redo?
-        private bool isLastWasUndo; // последняя операция была Undo?
-
         public Editor()
         {
             InitializeComponent();
+            paintingInAction = new PaintingInAction(buttonForMoving, buttonForRemoving, history, shapes);
             this.KeyPreview = true;
         }
 
         private void PictureBoxMouseDown(object sender, MouseEventArgs e)
         {
-            isPressed = true;
-            beginX = e.X;
-            beginY = e.Y;
-
-            if (!buttonForRemoving.Enabled) // если была нажата конпка удаления
-            {
-                foreach (Line line in shapes)
-                {
-                    if (GeometricCalculations.IsPointOnLine(line.FirstPoint, line.SecondPoint, beginX, beginY))
-                    {
-                        line.RemoveLine(shapes, history);
-                        pictureBox.Invalidate();
-                        break;
-                    }
-                }
-            }
-            else if (!buttonForMoving.Enabled) // если была нажата кнопка перемещения
-            {
-                foreach (Line line in shapes)
-                {
-                    if (GeometricCalculations.IsPointInPoint(line.FirstPoint, beginX, beginY) || GeometricCalculations.IsPointInPoint(line.SecondPoint, beginX, beginY))
-                    {
-                        line.PrepareLineToMove(shapes, history, ref beginX, ref beginY);
-                        colorOfChangeLine = line.Pen.Color; // запоминаем цвет перемещаемой линии
-                        widthOfChangeLine = (int)line.Pen.Width; // запоминаем толщину перемещаемой линии
-                        wasMovedLine = true; // линия изменяется
-                        break;
-                    }
-                }
-
-                buttonForMoving.Enabled = true;
-            }
+            paintingInAction.Update(shapes);
+            paintingInAction.DefineActionAndCallAppropriateMethods(e, pictureBox, color, width);
         }
 
         private void PictureBoxMouseMove(object sender, MouseEventArgs e)
         {
-            if (isPressed) // если была нажата кнопка мыши, значит мы рисуем линию, поэтому запоминаем ее последние координаты
-            {
-                endX = e.X;
-                endY = e.Y;
-                pictureBox.Invalidate();
-            }
-            else
-            {
-                lastX = endX;
-                lastY = endY;
-            }
+            paintingInAction.Update(shapes);
+            paintingInAction.CallLineBuilding(e, pictureBox);
         }
 
         private void PictureBoxMouseUp(object sender, MouseEventArgs e)
         {
-            isPressed = false;
-
-            if (lastX != endX || lastY != endY) // если линия не является точкой
-            {
-                if (wasMovedLine) // если линия была изменена, то добавляем линию с теми же параметрами, которые у нее были изначально
-                {
-                    shapes.Add(new Line(new Point(beginX, beginY), new Point(endX, endY), new Pen(colorOfChangeLine, widthOfChangeLine)));
-                    wasMovedLine = false;
-                }
-                else
-                {
-                    shapes.Add(new Line(new Point(beginX, beginY), new Point(endX, endY), new Pen(color, width)));
-                }
-
-                history.AddHistory(new CommandShape(shapes[shapes.Count - 1], "Добавление"), true);
-            }
+            paintingInAction.Update(shapes);
+            paintingInAction.AddLineInHistory();
         }
 
         private void PictureBoxPaint(object sender, PaintEventArgs e)
         {
-            if (!isLastWasUndo && !isLastWasRedo && buttonForClearing.Enabled && buttonForRemoving.Enabled)
-            {
-                if (wasMovedLine) // значит линия сейчас изменяется, поэтому рисуем ее, используя те цвет и толщину, которые у нее были изначально
-                {
-                    e.Graphics.DrawLine(new Pen(colorOfChangeLine, widthOfChangeLine), new Point(beginX, beginY), new Point(endX, endY));
-                }
-                else
-                {
-                    e.Graphics.DrawLine(new Pen(color, width), new Point(beginX, beginY), new Point(endX, endY));
-                }
-            }
-
+            paintingInAction.Update(shapes);
+            paintingInAction.RepaintAll(e);
+            buttonForMoving.Enabled = true;
             buttonForRemoving.Enabled = true;
-            buttonForClearing.Enabled = true;
-            isLastWasUndo = false;
-            isLastWasRedo = false;
-
-            foreach (Line line in shapes) // изображаем все линии
-            {
-                e.Graphics.DrawLine(line.Pen, line.FirstPoint, line.SecondPoint);
-            }
         }
 
         /// <summary>
@@ -159,7 +73,6 @@ namespace Editor
         {
             if (shapes.Count != 0)
             {
-                buttonForClearing.Enabled = false;
                 actions.RemoveAllShapes(ref shapes, history);
                 pictureBox.Invalidate();
             }
@@ -173,14 +86,12 @@ namespace Editor
         private void OnUndoClick(object sender, EventArgs e)
         {
             history.Undo(shapes);
-            isLastWasUndo = true;
             pictureBox.Invalidate();
         }
 
         private void OnRedoClick(object sender, EventArgs e)
         {
             history.Redo(shapes);
-            isLastWasRedo = true;
             pictureBox.Invalidate();
         }
 
---
+++ 3 semester/4 homework/Editor/Editor/PaintingInAction.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor
+{
+    /// <summary>
+    /// Отвечает за рисование на графическом окне в процессе
+    /// </summary>
+    public class PaintingInAction
+    {
+        private History history;
+        private List<Shape> shapes;
+        private ShapeBuilder shapeBuilder;
+
+        private int beginX; // координата начала линии по X
+        private int beginY; // координата начала линии по Y
+        private int endX; // координата конца линии по X
+        private int endY; // координата конца линии по Y
+
+        private Color colorOfMovedLine; // представляет цвет линии, которую перемещаем
+        private int widthOfMovedLine; // представляет толщину линии, которую перемещаем
+        
+        private bool isPressed; // была ли нажата кнопка
+        private bool wasMouseMove; // было ли перемещение мышки по элементу управления
+        private bool isLineDrawn; // рисуется ли линия
+
+        private Button buttonForMoving;
+        private Button buttonForRemoving;
+
+        public PaintingInAction(Button buttonForMoving, Button buttonForRemoving, History history, List<Shape> shapes)
+        {
+            this.buttonForMoving = buttonForMoving;
+            this.buttonForRemoving = buttonForRemoving;
+            this.history = history;
+            this.shapes = shapes;
+        }
+
+        /// <summary>
+        /// Обновляет ссылку, чтобы поле класса PaintingInAction продолжало ссылаться на соответствующее поле класса Editor
+        /// </summary>
+        public void Update(List<Shape> shapes)
+        {
+            this.shapes = shapes;
+        }
+
+        /// <summary>
+        /// Определяет, какая кнопка была нажата и вызывает соответствующие методы. 
+        /// Если никакая не нажата, то подготоваливает линию к построению
+        /// </summary>
+        public void DefineActionAndCallAppropriateMethods(MouseEventArgs e, PictureBox pictureBox, Color color, int width)
+        {
+            isPressed = true;
+            wasMouseMove = false;
+            beginX = e.X;
+            beginY = e.Y;
+
+            if (!buttonForRemoving.Enabled)
+            {
+                foreach (Line line in shapes)
+                {
+                    if (GeometricCalculations.IsPointOnLine(line.FirstPoint, line.SecondPoint, beginX, beginY))
+                    {
+                        line.RemoveLine(shapes, history);
+                        pictureBox.Invalidate();
+                        break;
+                    }
+                }
+            }
+            else if (!buttonForMoving.Enabled)
+            {
+                foreach (Line line in shapes)
+                {
+                    if (GeometricCalculations.IsPointInPoint(line.FirstPoint, beginX, beginY) || GeometricCalculations.IsPointInPoint(line.SecondPoint, beginX, beginY))
+                    {
+                        line.PrepareLineToMove(shapes, history, ref beginX, ref beginY);
+                        colorOfMovedLine = line.Pen.Color; // запоминаем цвет перемещаемой линии
+                        widthOfMovedLine = (int)line.Pen.Width; // запоминаем толщину перемещаемой линии
+                        shapeBuilder = new ShapeBuilder(colorOfMovedLine, widthOfMovedLine);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                shapeBuilder = new ShapeBuilder(color, width);
+            }
+        }
+
+        /// <summary>
+        /// Вызывает метод для построения линии
+        /// </summary>
+        public void CallLineBuilding(MouseEventArgs e, PictureBox pictureBox)
+        {
+            if (isPressed) // если была нажата кнопка мыши, значит мы рисуем линию, поэтому запоминаем ее последние координаты
+            {
+                endX = e.X;
+                endY = e.Y;
+                isLineDrawn = true;
+                pictureBox.Invalidate();
+            }
+
+            wasMouseMove = true;
+        }
+
+        /// <summary>
+        /// Добавляет линию, которая до опускания мыши рисовалась, в историю. Если линия не рисовалась, то ничего не делает
+        /// </summary>
+        public void AddLineInHistory()
+        {
+            isPressed = false;
+
+            if (wasMouseMove) // если линия не является точкой
+            {
+                if (!buttonForMoving.Enabled) // если линия была изменена, то добавляем линию с теми же параметрами, которые у нее были изначально
+                {
+                    shapes.Add(new Line(new Point(beginX, beginY), new Point(endX, endY), new Pen(colorOfMovedLine, widthOfMovedLine)));
+                }
+                else
+                {
+                    Color color = shapeBuilder.Color;
+                    int width = shapeBuilder.Width;
+                    shapes.Add(new Line(new Point(beginX, beginY), new Point(endX, endY), new Pen(color, width)));
+                }
+
+                history.AddHistory(new CommandShape(shapes[shapes.Count - 1], "Добавление"), true);
+            }
+        }
+
+        /// <summary>
+        /// Перерисовывает элемент управления
+        /// </summary>
+        public void RepaintAll(PaintEventArgs e)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw(e);
+            }
+
+            if (isLineDrawn)
+            {
+                shapeBuilder.BuildLine(new Point(beginX, beginY), new Point(endX, endY), e);
+                isLineDrawn = false;
+            }
+        }
+    }
+}
