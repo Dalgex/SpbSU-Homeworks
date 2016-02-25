38198a6612b44a6d025d765544eea03549510437
Valeriy Karnaukhov <Valera-96@yandex.ru>
2016-02-25 20:57:27 +0300

    Tasks 1

---
+++ 4 semester/1 homework/Task 1/Task 1/Program.fs
@@ -0,0 +1,17 @@
+﻿let factorial n = List.fold(fun x acc -> x * acc) 1 [1..n]
+
+let rec fibonacci n =
+    if (n < 2)
+    then n
+    else fibonacci(n - 1) + fibonacci(n - 2)
+
+let rec reverseList list = 
+    match list with
+    | head::tail -> List.append (reverseList tail) [head]
+    | [] -> []
+
+let formList number = 
+    let powersList n = 
+        let powerOfTwo value = pown 2 value
+        List.init n (fun index -> powerOfTwo (number + index))
+    powersList 5
