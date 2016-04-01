281a53cfb3b08c523b7cfb7519663519a592fb1c
Valeriy Karnaukhov <Valera-96@yandex.ru>
2016-04-01 12:54:09 +0300

    Test 1

---
+++ 4 semester/Test work/Test 1/Test 1/Program.fs
@@ -0,0 +1,9 @@
+﻿let seqOfOnes = Seq.initInfinite (fun index -> if (index % 2) = 1 then -1 else 1)
+
+let seqOfNaturalNumbers = Seq.initInfinite (fun index -> index + 1)
+
+let seqOfPairs = Seq.zip seqOfOnes seqOfNaturalNumbers
+
+let resultSeq = Seq.map (fun (x, y) -> x * y) seqOfPairs
+
+printfn "Последовательность натуральных чисел, где каждое второе умножено на -1:\n%A" resultSeq
