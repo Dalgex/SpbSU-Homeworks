5808a4331bba02f9d226260cdddcf980312f8251
Valeriy Karnaukhov <Valera-96@yandex.ru>
2016-05-18 19:40:45 +0300

    Sixth homework

---
+++ 4 semester/6 homework/Task 6.1/Task 6.1/Program.fs
@@ -0,0 +1,17 @@
+﻿type RoundingBuilder(precision : int) =
+    member this.Bind(x : float, f : float -> float) =
+        f (System.Math.Round(x, precision))
+    member this.Return(x : float) =
+        System.Math.Round(x, precision)
+
+let rounding precision = RoundingBuilder(precision)
+
+let test =
+    rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+
+printfn "%A" test
+    
---
+++ 4 semester/6 homework/Task 6.2/Task 6.2/Program.fs
@@ -0,0 +1,27 @@
+﻿type StringBuilder() = 
+    member this.Bind(str, f) = 
+        match System.Int32.TryParse(str) with
+        | (true, int) -> f int
+        | _ -> None
+     member this.Return(x) = Some(x)
+
+let strexpr = StringBuilder()
+
+let test1 = 
+    strexpr {
+        let! x = "1"
+        let! y = "2"
+        let z = x + y
+        return z
+    }
+
+let test2 = 
+    strexpr {
+        let! x = "1"
+        let! y = "b"
+        let z = x + y
+        return z
+    }
+
+printfn "%A" test1
+printfn "%A" test2
